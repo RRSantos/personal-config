24be7cda66344a4ba30aa18a3fa73a7944033eb0
Rodrigo Ramos dos Santos <rodrigo.ramos.santos@gmail.com>
2014-05-03 01:24:40 -0300

    windows automate install

---
+++ vim/config-vim.ps1
@@ -0,0 +1,13 @@
+#Script to customize gvim
+$rcFiles = "windows\*"
+$vimFiles = @("colors", "doc", "ftdetect", "ftplugin", "indent", "syntax")
+$userPath =  $env:userprofile
+$vimUserConfigPath = "$userPath.vim"
+
+Copy-Item $rcFiles -Destination $userPath -force
+
+if (-not (Test-Path $vimUserConfigPath)) {mkdir $vimUserConfigPath}
+
+Copy-Item $vimFiles -Destination $vimUserConfigPath -Recurse
+
+
